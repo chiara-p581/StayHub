d5b3828d6fc668f5480512dcb49b8179edf777e6
matilameijeiras <matilameijeiras@gmail.com>
2026-08-24 11:17:40 -0300

    Agrega ServicioDePagos: capas presentacion/negocio/datos, patrones Facade/Adapter/DAO

---
+++ proyecto/src/main/java/com/stayhub/pagos/exception/CodigoErrorPago.java
@@ -0,0 +1,8 @@
+package com.stayhub.pagos.exception;
+
+public enum CodigoErrorPago {
+    SOLICITUD_INVALIDA,
+    PAGO_NO_ENCONTRADO,
+    PAGO_RECHAZADO,
+    ERROR_COMUNICACION_PASARELA
+}
---
+++ proyecto/src/main/java/com/stayhub/pagos/model/EstadoPago.java
@@ -0,0 +1,7 @@
+package com.stayhub.pagos.model;
+
+public enum EstadoPago {
+    PENDIENTE,
+    APROBADO,
+    RECHAZADO
+}
